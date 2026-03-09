739ace443abc3268a86ba4cc5868c0cd89306b70
Nikke-coder <niklas.isaksson@targetflow.fi>
2026-03-09 21:37:06 +0200

    Update supabase.js

--- src/supabase.js
+++ src/supabase.js
@@ -1,5 +1,5 @@
 import { createClient } from '@supabase/supabase-js'
-const URL = 'https://jzqgndciukggcwthxyrv.supabase.co'
+const URL = 'https://jzqgndcrukggcwthxyrv.supabase.co'
 const KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imp6cWduZGNydWtnZ2N3dGh4eXJ2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzI5NTA3NDIsImV4cCI6MjA4ODUyNjc0Mn0.6nSM1D1P36Did6pT27IBvO-tSQ2ihSrxhlZLlaEhvEc'
 export const supabase = createClient(URL, KEY)
 export const CLIENT = 'stremet'
